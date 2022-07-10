5c2f6ffcc767290b26da046b70d8751b56ac378d
dev-yoga <spin_shoppe@icloud.com>
2022-07-10 14:57:13 -0700

    Test

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("I am a response")
+  context = {"name": "Pupcake"}
+  template = loader.get_template("app/home.html")
+  return HttpResponse(template.render(context))
